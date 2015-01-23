85e26da4c59eeb661d34e7182c587c1c2ab32b04
necessary-ambiguity <necessary-ambiguity@email.not.provided>
2015-01-22 19:27:18 -0700

    Finished tidying column names

--- run_analysis.R
+++ run_analysis.R
@@ -4,7 +4,7 @@
 
 #source("getdata.R")
 
-main <- function() {
+main <- function(forceRead=FALSE) {
 	temporaryfile <- "mergeddata.csv"
 	getdata <- function() {
 		testname <- "_test"
@@ -19,15 +19,21 @@ main <- function() {
 		featurename <- "feature"
 
 		readactivities <- function() {
+	# Get the list of activity labels
 			filename <- "activity_labels.txt"
 			columns <- c(idactivity, activityname)
-			read.table(filename, sep=" ", col.names=columns)
+			df <- read.table(filename, sep=" ", col.names=columns)
+	# Tidy the activity names
+			df[, activityname] <- gsub("_", "", tolower(df[, activityname]), fixed=TRUE)
+			df
+
 		}
 		readfeatures <- function() {
+	# Get the list of features (names of the measurement columns)
 			filename <- "features.txt"
 			columns <- c(idfeature, featurename)
-			features <- read.table(filename, sep=" ", col.names=columns)
-			features$feature
+			df <- read.table(filename, sep=" ", col.names=columns)
+			df[, featurename]
 		}
 		readdatasets <- function(setname, activities, features) {
 			subjectsname <- "subject"
@@ -35,14 +41,18 @@ main <- function() {
 			xname <- "X"
 			ext <- ".txt"
 
+	# Read in the subjects (single column)
 			columns <- c(idsubject)
 			subjects <- read.table(paste0(subjectsname, setname, ext), col.names=columns)
 
+	# Read in the activities (single column)
 			columns <- c(idactivity)
 			idactivities <- read.table(paste0(activitiesname, setname, ext), col.names=columns)
 
+	# Read in the measurements (many columns)
 			xvalues <- read.table(paste0(xname, setname, ext), col.names=features)
 
+	# Combine all the columns into a single data frame
 			xvalues <- cbind(idactivities, xvalues)
 			xvalues <- cbind(subjects, xvalues)
 
@@ -72,13 +82,6 @@ main <- function() {
 		}
 		dfresults <- extractrelevantmeasurements(dfresults)
 
-	# Appropriately labels the data set with descriptive variable names.
-		assigndescriptivenames <- function(df) {
-			names(df) <- gsub(".", "", tolower(names(df)), fixed=TRUE)
-			df
-		}
-		dfresults <- assigndescriptivenames(dfresults )
-
 		write.table(dfresults, file=temporaryfile, row.names=FALSE)
 		dfresults
 	}
@@ -89,6 +92,24 @@ main <- function() {
 		dfresults <- read.table(temporaryfile, header=TRUE)
 	}
 
+	# Appropriately labels the data set with descriptive variable names.
+	assigndescriptivenames <- function(df) {
+		names(df) <- gsub(".", "", tolower(names(df)), fixed=TRUE)
+		names(df) <- gsub("tbody", "timebody", names(df), fixed=TRUE)
+		names(df) <- gsub("tgravity", "timegravity", names(df), fixed=TRUE)
+		names(df) <- gsub("fbody", "frequencybody", names(df), fixed=TRUE)
+		names(df) <- gsub("fgravity", "frequencygravity", names(df), fixed=TRUE)
+
+		names(df) <- gsub("acc", "accelerometer", names(df), fixed=TRUE)
+		names(df) <- gsub("gyro", "gyroscope", names(df), fixed=TRUE)
+		names(df) <- gsub("mag", "magnitude", names(df), fixed=TRUE)
+		
+		names(df) <- gsub("std", "standarddeviation", names(df), fixed=TRUE)
+		df
+	}
+	dfresults <- assigndescriptivenames(dfresults )
+
+
 	# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 	tidydata <- function(df) {
 		aggregate(x=df[,3:ncol(df)], by=list(subject=df$idsubject, activity=df$activity), FUN="mean")
@@ -98,6 +119,6 @@ main <- function() {
 	dfresults
 }
 
-dftidy <- main()
+dftidy <- main(TRUE)
 filename <- "means_by_activity_and_subject.txt"
 write.table(dftidy, file=filename, row.names=FALSE)
